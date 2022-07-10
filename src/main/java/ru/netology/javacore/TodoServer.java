a471ff490534a67abd5524cf3153c8b50648aac7
Alexander Kozhin <hustler77@mail.ru>
2022-07-10 21:39:18 +0300

    Modificate TodoServer

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,6 +1,10 @@
 package ru.netology.javacore;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -24,8 +28,13 @@ public class TodoServer {
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                      PrintWriter out = new PrintWriter(socket.getOutputStream())) {
                     String json = in.readLine();
-                    Gson gson = new Gson();
-                    TaskRequest request = gson.fromJson(json, TaskRequest.class);
+                    JSONParser parser = new JSONParser();
+                    Object obj = parser.parse(json);
+                    JSONObject jsonObject = (JSONObject) obj;
+                    GsonBuilder builder = new GsonBuilder();
+                    Gson gson = builder.create();
+                    TaskRequest request = gson.fromJson(jsonObject.toString(), TaskRequest.class);
+
                     switch (request.getType()) {
                         case "ADD":
                             todos.addTask(request.getTask());
@@ -38,7 +47,7 @@ public class TodoServer {
                     out.println(todos.getAllTasks());
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             System.out.println("Не могу стартовать сервер");
             e.printStackTrace();
         }
