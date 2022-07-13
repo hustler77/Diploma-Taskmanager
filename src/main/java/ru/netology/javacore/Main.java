22ec6e56495bee8b4a8abb85198de3f1741c0966
Alexander Kozhin <hustler77@mail.ru>
2022-07-13 21:51:57 +0300

    change port number

--- src/main/java/ru/netology/javacore/Main.java
+++ src/main/java/ru/netology/javacore/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Todos todos = new Todos();
-        TodoServer server = new TodoServer(8888, todos);
+        TodoServer server = new TodoServer(8989, todos);
         server.start();
     }
 }
